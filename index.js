18d6f0e944b6cf53c4e42b0fee6aeaede8ee41c3
Eyal Michon <eyalmichon1@gmail.com>
2021-06-08 22:13:05 +0300

    🕛 Add going all over unread messages so no message is missed. Get automatically reconnected if disconnected.

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const wa = require('@open-wa/wa-automate');
-const msgHandler = require('./handler/index');
+const msgHandler = require('./handler');
 
 wa.create({
-    sessionId: "HELPER",
-    authTimeout: 60, //wait only 60 seconds to get a connection with the host account device
-    blockCrashLogs: true,
-    disableSpins: true,
+    // run instead of chromium
+    // executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+    sessionId: 'TheMultitasker',
+    // authTimeout: 60, //wait only 60 seconds to get a connection with the host account device
     headless: true,
-    hostNotificationLang: 'EN-GB',
-    logConsole: false,
-    popup: true,
-    qrTimeout: 0, //0 means it will wait forever for you to scan the qr code
+    autoRefresh: true,
+    cacheEnabled: false,
+    useChrome: true
+    // qrTimeout: 0, //0 means it will wait forever for you to scan the qr code
 }).then(client => start(client));
 
 async function start(client) {
+    console.log('The Multitasker', '[Version 1.1]')
+
+    // Force it to keep the current session
+    client.onStateChanged((state) => {
+        console.log('[Client State]', state)
+        if (state === 'CONFLICT' || state === 'DISCONNECTED') client.forceRefocus()
+    })
+
+    // Get all unread messages and go over them.
+    const unreadMessages = await client.getAllUnreadMessages();
+    unreadMessages.forEach(message => {
+        msgHandler(client, message);
+    });
     client.onAnyMessage(message => {
         // // Cut message Cache if cache more than 3K
         client.getAmountOfLoadedMessages().then((msg) => (msg >= 3000) && client.cutMsgCache())
