a19365aaece7d706604f97e671809da7bbb805a5
Eyal Michon <eyalmichon1@gmail.com>
2021-06-12 11:22:11 +0300

    🐛 a bug in function cutMessageCache caused unexpected result such as freezing, so it's been removed.

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ const wa = require('@open-wa/wa-automate');
 const msgHandler = require('./handler');
 
 wa.create({
-    // run instead of chromium
-    // executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
     sessionId: 'TheMultitasker',
-    // authTimeout: 60, //wait only 60 seconds to get a connection with the host account device
     headless: true,
     autoRefresh: true,
     cacheEnabled: false,
     useChrome: true
-    // qrTimeout: 0, //0 means it will wait forever for you to scan the qr code
+    // run instead of chromium
+    // executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
 }).then(client => start(client));
 
 async function start(client) {
-    console.log('The Multitasker', '[Version 1.1]')
+    console.log('The Multitasker', '[Version 1.1.1]')
 
     // Force it to keep the current session
     client.onStateChanged((state) => {
@@ -28,9 +26,6 @@ async function start(client) {
         msgHandler(client, message);
     });
     client.onAnyMessage(message => {
-        // // Cut message Cache if cache more than 3K
-        client.getAmountOfLoadedMessages().then((msg) => (msg >= 3000) && client.cutMsgCache())
-
         // Message Handler
         msgHandler(client, message);
     });
