7c3bbf66aad4762704112ed6bc71a0ff3a4c43db
Your Name <you@example.com>
2021-06-26 20:51:21 +0300

    Add spam, fetcher, compile, senders for use in maessage handler.

--- handler/lib/index.js
+++ handler/lib/index.js
@@ -1,4 +1,7 @@
 exports.downloader = require('./downloader');
 exports.meme = require('./meme');
 exports.redAlerts = require('./redAlerts');
-exports.senders = require('./senders');
+exports.senders = require('./util/senders');
+exports.fetcher = require('./fetcher');
+exports.compile = require('./compile');
+exports.spam = require('./util/spam');
