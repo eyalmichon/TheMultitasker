b2eafdabd80beddb37f87a0f1e6b7b14967c1c2d
Your Name <you@example.com>
2021-06-26 20:46:47 +0300

    Move link fetching logic into here from the message handler.

--- handler/lib/downloader.js
+++ handler/lib/downloader.js
@@ -1,5 +1,6 @@
 const { promisify } = require('util')
-const { instagram, twitter } = require('video-url-link');
+const { twitter, instagram } = require('video-url-link');
+const { fetchHead } = require('./fetcher')
 
 const igGetInfo = promisify(instagram.getInfo);
 const twtGetInfo = promisify(twitter.getInfo);
@@ -8,13 +9,27 @@ const twtGetInfo = promisify(twitter.getInfo);
 /**
  * Get Instagram Metadata using video-url-link
  *
- * @param  {String} url The instagram URL.
+ * @param  {String} url The Instagram URL.
  */
 const insta = (url) => new Promise((resolve, reject) => {
     const uri = url.replace(/\?.*$/g, '')
     igGetInfo(uri, {})
-        // return the variants list.
-        .then((result) => resolve(result['list']))
+        .then((result) => {
+            const promises = [];
+            // Then go over the returned list of videos/images.
+            result['list'].forEach(item => {
+                // If there is a video key in the JSON, get the video.
+                if (item['video'] !== undefined) {
+                    if (fetchHead(item['video']) !== 'CONTENT-TOO-LARGE')
+                        promises.push(item['video']);
+                }
+                // Else if the requested item didn't have a video in it.
+                else
+                    promises.push(item['image'])
+            })
+
+            resolve(Promise.all(promises));
+        })
         .catch((err) => {
             console.error(err)
             reject(err)
@@ -29,7 +44,19 @@ const insta = (url) => new Promise((resolve, reject) => {
 const tweet = (url) => new Promise((resolve, reject) => {
     twtGetInfo(url, {})
         // return the variants list.
-        .then((content) => resolve(content['variants']))
+        .then((content) => content['variants'])
+        .then((data) => {
+            let link;
+            let bitrate = 0;
+            // Then go over the returned list of videos and find the max bitrate video.
+            data.forEach(item => {
+                if (item['bitrate'] !== undefined && bitrate < item['bitrate']) {
+                    bitrate = item['bitrate'];
+                    link = item['url'];
+                }
+            });
+            resolve(link);
+        })
         .catch((err) => {
             console.error(err)
             reject(err)
