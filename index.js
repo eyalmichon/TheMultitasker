276ecf778083f5be8825bc756b798e0cfcff8c71
Your Name <you@example.com>
2021-06-26 20:43:50 +0300

    Add message handling for if the browser gets stuck and reconnected later (so you won't loose any unread messages. Add an interval for refreshing the session every 24 hours.

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
 const wa = require('@open-wa/wa-automate');
 const msgHandler = require('./handler');
 
+// Get all unread messages and go over them.
+async function handleUnread(client) {
+    const unreadMessages = await client.getAllUnreadMessages();
+    unreadMessages.forEach(message => {
+        msgHandler(client, message).catch(err => {
+            console.error(err);
+        })
+    });
+}
+
 wa.create({
     sessionId: 'TheMultitasker',
     headless: true,
-    autoRefresh: true,
     cacheEnabled: false,
-    useChrome: true
+    // try to auto detect chrome location.
+    // useChrome: true
     // run instead of chromium
-    // executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+    executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+    // for heroku
+    // executablePath: '/app/.apt/usr/bin/google-chrome',
 }).then(client => start(client));
 
 async function start(client) {
-    console.log('The Multitasker', '[Version 1.1.2]')
+    console.log('The Multitasker', '[Version 1.2]')
+
+    // refresh the client every 24 hours.
+    setInterval(() => {
+        client.refresh();
+    }, 86400000)
 
     // Force it to keep the current session
     client.onStateChanged((state) => {
         console.log('[Client State]', state)
-        if (state === 'CONFLICT' || state === 'DISCONNECTED') client.forceRefocus()
+        if (state === 'CONNECTED') handleUnread(client);
+        if (state === 'CONFLICT' || state === 'DISCONNECTED') client.forceRefocus();
     })
 
-    // Get all unread messages and go over them.
-    const unreadMessages = await client.getAllUnreadMessages();
-    unreadMessages.forEach(message => {
-        msgHandler(client, message);
-    });
+    await handleUnread(client);
+
     client.onAnyMessage(message => {
         // Message Handler
         msgHandler(client, message);
-    });
+    }).catch(err => {
+        console.error(err);
+    })
 }
