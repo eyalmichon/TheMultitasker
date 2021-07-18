754f28520d07d120ab823415e2f3fc257877bcbd
Eyal <eyalmichon1@gmail.com>
2021-07-19 01:38:01 +0300

    Add fetchToFile function for fetching content into a local file.

--- handler/lib/fetcher.js
+++ handler/lib/fetcher.js
@@ -1,5 +1,16 @@
 const fetch = require('node-fetch');
+const fs = require('fs');
 const AbortController = require('abort-controller');
+const MAX_SIZE_ALLOWED = 47185920;
+
+function getRandomFileName() {
+    var timestamp = new Date().toISOString().replace(/[-:.Z]/g, "");
+    var random = ("" + Math.random()).substring(2, 6);
+    var random_number = timestamp + random;
+    return random_number;
+}
+
+
 /**
  *Fetch Json from Url
  *
@@ -16,16 +27,20 @@ const fetchJson = (url, options) =>
                 reject(err)
             })
     )
-
+/**
+ * Check if the file located at the url is too large to handle by trying to get the content-length from the header.
+ * @param {*} url url of file
+ * @returns CONTENT_TOO_LARGE if above MAX_SIZE_ALLOWED, OK if below, NO_CONTENT_LENGTH if there was no content-length in header.
+ */
 const fetchHead = (url) => {
     return fetch(url, { method: 'HEAD' })
         .then(response => {
-            // return error if more than 30MB
-            if (response.headers.get('content-length') > 31457280) return 'CONTENT-TOO-LARGE';
-            else return true;
-        }).catch(err=>{
+            // return error if more than 45MB
+            if (response.headers.get('content-length') > MAX_SIZE_ALLOWED) return 'CONTENT_TOO_LARGE';
+            else return 'OK';
+        }).catch(err => {
             console.error(err)
-            return 'NO-CONTENT-LENGTH';
+            return 'NO_CONTENT_LENGTH';
         })
 }
 /**
@@ -52,7 +67,6 @@ const fetchText = (url, options) => {
  * Fetch base64 from url
  * @param {String} url
  */
-
 const fetchBase64 = (url, mimetype) => {
     return new Promise((resolve, reject) => {
         // console.log('Get base64 from:', url)
@@ -90,10 +104,37 @@ const fetchWithTimeout = async (url, requestOptions, timeout) => {
 }
 
 
+/**
+ * 
+ * @param {*} url URL of file.
+ * @param {*} extension extension of file.
+ * @returns a Promise which holds {status, filePath}
+ */
+const fetchToFile = (url, extension) => new Promise((resolve, reject) => {
+    const fileName = getRandomFileName();
+    const filePath = `./handler/tmp/${fileName}.${extension}`;
+    fetch(url).then(res => {
+        if (res.status === 200) {
+
+            res.body.pipe(fs.createWriteStream(filePath))
+                .on('error', () => reject('error'))
+                .on('finish', () => resolve({ status: res.status, filePath: filePath }))
+        }
+        else {
+            resolve({ status: res.status, filePath: null });
+        }
+    }).catch(err => {
+        console.error(err);
+        reject(err);
+    });
+})
+
 module.exports = {
     fetchJson,
     fetchText,
     fetchBase64,
     fetchWithTimeout,
-    fetchHead
+    fetchHead,
+    fetchToFile,
+    MAX_SIZE_ALLOWED
 }
