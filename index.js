3e6e11727755ffcc0524aa43ab08ebf1d1b75a79
Eyal <eyalmichon1@gmail.com>
2021-07-19 01:44:36 +0300

    Add restartOnCrash to handle crashes better, add chrome settings.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const wa = require('@open-wa/wa-automate');
+const { create, Client } = require('@open-wa/wa-automate');
 const msgHandler = require('./handler');
 
 // Get all unread messages and go over them.
@@ -11,39 +11,73 @@ async function handleUnread(client) {
     });
 }
 
-wa.create({
+
+
+const start = async (client = new Client()) => {
+    console.log('The Multitasker', '[Version 1.3]')
+
+    // // refresh the client every hour.
+    // setInterval(() => {
+    //     client.refresh();
+    // }, 3600000)
+
+    try {
+
+        // Force it to keep the current session
+        client.onStateChanged(state => {
+            console.log('[Client State]', state)
+            if (state === 'CONNECTED') handleUnread(client);
+            if (state === 'CONFLICT' || state === 'DISCONNECTED') client.forceRefocus();
+        })
+
+        handleUnread(client);
+
+        client.onAnyMessage(message => {
+            // Message Handler
+            msgHandler(client, message);
+        }).catch(err => {
+            console.error(err);
+        })
+
+    } catch (err) {
+        console.error(err);
+        setTimeout(() => {
+            return client.kill()
+        }, 5000);
+    }
+}
+
+const options = {
     sessionId: 'TheMultitasker',
+    qrTimeout: 0,
+    authTimeout: 0,
     headless: true,
     cacheEnabled: false,
+    restartOnCrash: start,
+    killProcessOnBrowserClose: true,
+    // log browser errors
+    logConsoleErrors: true,
     // try to auto detect chrome location.
     // useChrome: true
     // run instead of chromium
     executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+    // for linux
+    // executablePath: '/usr/bin/google-chrome-stable',
     // for heroku
     // executablePath: '/app/.apt/usr/bin/google-chrome',
-}).then(client => start(client));
+    throwErrorOnTosBlock: false,
+    chromiumArgs: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--aggressive-cache-discard',
+        '--disable-cache',
+        '--disable-gl-drawing-for-tests',
+        '--disable-application-cache',
+        '--disable-offline-load-stale-cache',
+        '--disk-cache-size=0'
+    ]
+};
 
-async function start(client) {
-    console.log('The Multitasker', '[Version 1.2]')
-
-    // refresh the client every 24 hours.
-    setInterval(() => {
-        client.refresh();
-    }, 86400000)
-
-    // Force it to keep the current session
-    client.onStateChanged((state) => {
-        console.log('[Client State]', state)
-        if (state === 'CONNECTED') handleUnread(client);
-        if (state === 'CONFLICT' || state === 'DISCONNECTED') client.forceRefocus();
-    })
-
-    await handleUnread(client);
-
-    client.onAnyMessage(message => {
-        // Message Handler
-        msgHandler(client, message);
-    }).catch(err => {
-        console.error(err);
-    })
-}
+create(options)
+    .then(client => start(client))
+    .catch((err) => console.error(err));
