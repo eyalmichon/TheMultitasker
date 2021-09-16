cd2da08aa0d3145b2fe14c2162cac82fb948a5ff
Eyal <eyalmichon1@gmail.com>
2021-09-16 13:30:48 +0300

    ✨ Sticker to image.

--- handler/lib/commands/media.js
+++ handler/lib/commands/media.js
@@ -14,6 +14,10 @@ class Media {
         commands.removebg = this.addInfo(this.removeBG)
         commands.rmbg = this.alias(this.removeBG)
 
+        commands.toimage = this.addInfo(this.toImage)
+        commands.image = this.alias(this.toImage)
+        commands.img = this.alias(this.toImage)
+
         commands.videotomp3 = this.addInfo(this.videoTomp3)
         commands.v2mp3 = this.alias(this.videoTomp3)
         commands.v2m = this.alias(this.videoTomp3)
@@ -29,28 +33,71 @@ class Media {
             // if there is a quoted message, use it to make the sticker.
             if (quoted) message = message.quotedMsg;
 
-            if (message.type !== 'image' && quoted && ogMsg.type !== 'image') return errors.NOT_IMG
+            if (message.type !== 'image' && message.type !== 'sticker' && ogMsg.type !== 'image') return errors.NOT_IMG
 
             return decryptMedia(message).then(async buffer => {
-                let base64 = buffer.toString('base64');
+                if (message.type === 'sticker')
+                    buffer = await imageProcessing.sharp(buffer).toFormat('png').toBuffer()
 
-                if (options.bg || !!options.bgurl) {
+                if (!!options.bg || !!options.bgurl) {
                     if (quoted)
                         options.bg = await decryptMedia(ogMsg);
-                    base64 = await imageProcessing.removeBG(base64, options);
+                    buffer = await imageProcessing.removeBG(buffer, options);
                 }
                 else
-                    base64 = await imageProcessing.removeBG(base64);
+                    buffer = await imageProcessing.removeBG(buffer, options);
                 if (stroke)
-                    base64 = await imageProcessing.addStroke(base64, options)
+                    buffer = await imageProcessing.addStroke(buffer, options)
                 if (text)
-                    base64 = await imageProcessing.addText(base64, options)
+                    buffer = await imageProcessing.addText(buffer, options)
 
+                let base64 = buffer.toString('base64');
                 return returnType.sendFile(`data:document/png;base64,${base64}`, `the_multitasker.png`, '', false)
             })
         },
         help: () => help.Media.removebg
     }
+    toImage = {
+        func: (args, message) => {
+            const options = parser.parse(args);
+            const quoted = !!message.quotedMsg;
+            const ogMsg = message;
+            let rmbg = !!options.r || !!options.rb;
+            let stroke = !!options.s || !!options.stroke;
+            let text = !!options.t || !!options.text;
+            let file = !!options.f || !!options.file;
+            // if there is a quoted message, use it to make the sticker.
+            if (quoted) message = message.quotedMsg;
+
+            if (message.type !== 'image' && message.type !== 'sticker' && ogMsg.type !== 'image') return errors.NOT_IMG
+
+            return decryptMedia(message).then(async buffer => {
+                if (message.type === 'sticker')
+                    buffer = await imageProcessing.sharp(buffer).toFormat('png').toBuffer()
+
+                if (rmbg) {
+                    if (!!options.bg || !!options.bgurl) {
+                        if (quoted)
+                            options.bg = await decryptMedia(ogMsg);
+                        buffer = await imageProcessing.removeBG(buffer, options);
+                    }
+                    else
+                        buffer = await imageProcessing.removeBG(buffer, options);
+                }
+                if (stroke)
+                    buffer = await imageProcessing.addStroke(buffer, options)
+                if (text)
+                    buffer = await imageProcessing.addText(buffer, options)
+
+                let base64 = buffer.toString('base64');
+                if (file) return returnType.sendFile(`data:document/png;base64,${base64}`, `the_multitasker.png`, '', false)
+                else return returnType.sendFile(`data:image/png;base64,${base64}`, `the_multitasker.png`, '', false)
+            })
+        },
+        help: () => help.Media.removebg
+    }
+
+
 
     videoTomp3 = {
         func: (args, message) => {
