3f15aff0429a4ac918d0ba3abb6cc6751ee89090
Eyal <eyalmichon1@gmail.com>
2021-12-01 11:56:02 +0200

    small change: add handling objects in sender.

--- handler/util/senders.js
+++ handler/util/senders.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path')
 const sendersPath = path.join(__dirname, 'senders.json');
+const { languages } = require('./localizations.json');
 
 class Senders {
     constructor() {
@@ -11,6 +12,7 @@ class Senders {
             fs.writeFileSync(sendersPath, `{
                 "Me": "Enter your number here -> ******@c.us",
                 "Allowed": [],
+                "WelcomeMsg": {},
                 "RedAlerts-MessageOnly": [],
                 "RedAlerts": [],
             }`)
@@ -34,36 +36,67 @@ class Senders {
         return array.filter(item => item !== element);
     }
 
+    // Get the type of an object.
+    TypeOf(obj) {
+        return Object.prototype.toString.call(obj).slice(8, -1);
+    }
+
     /**
      * Add a sender to the senders.json file.
-     * @param group The group which we want to add to.
-     * @param number The phone number which we want to add.
-     * @returns true if added successfully or if was already there, false is the group doesn't 
+     * @param group The group which you want to add to.
+     * @param number The phone number which you want to add.
+     * @param lang The language of the group.
+     * @returns true if added successfully or if was already there, false if the group doesn't 
      * exist or the number entered was incorrect or 'Me' group was selected.
      */
-    addSender(group, number) {
-        if (this.senders[group] === undefined || !this.isCorrectNumber(number) || group === 'Me')
+    addSender(group, number, lang) {
+        const _group = this.senders[group];
+        if (!_group || !this.isCorrectNumber(number) || group === 'Me')
             return false;
-        if (!this.senders[group].includes(number)) {
-            this.senders[group].push(number);
-            fs.writeFileSync(sendersPath, JSON.stringify(this.senders));
+
+        if (this.TypeOf(_group) === 'Object') {
+            // if it's not there already, add it.
+            if (!_group[number] && !!lang && languages.includes(lang)) {
+                this.senders[group] = lang;
+                fs.writeFileSync(sendersPath, JSON.stringify(this.senders));
+            }
+            else return false
         }
+        else
+            // if it's not there already, add it.
+            if (!_group.includes(number)) {
+                this.senders[group].push(number);
+                fs.writeFileSync(sendersPath, JSON.stringify(this.senders));
+            }
+            else return false
+
         return true;
     }
     /**
      * Remove a sender from the senders.json file.
-     * @param group The group which we want to remove from.
-     * @param number The phone number which we want to remove.
-     * @returns true if removed or not found, false is the group doesn't 
+     * @param group The group which you want to remove from.
+     * @param number The phone number which you want to remove.
+     * @returns true if removed or not found, false if the group doesn't 
      * exist or the number entered was incorrect or 'Me' group was selected.
      */
     removeSender(group, number) {
-        if (this.senders[group] === undefined || !this.isCorrectNumber(number) || group === 'Me')
+        const _group = this.senders[group];
+        if (!_group || !this.isCorrectNumber(number) || group === 'Me')
             return false;
-        if (this.senders[group].includes(number)) {
-            this.senders[group] = this.removeFromArray(this.senders[group], number);
-            fs.writeFileSync(sendersPath, JSON.stringify(this.senders));
+
+        if (this.TypeOf(_group) === 'Object') {
+            if (_group[number]) {
+                delete this.senders[group]
+                fs.writeFileSync(sendersPath, JSON.stringify(this.senders));
+            }
+            else return false
         }
+        else
+            if (_group.includes(number)) {
+                this.senders[group] = this.removeFromArray(_group, number)
+                fs.writeFileSync(sendersPath, JSON.stringify(this.senders));
+            }
+            else return false
         return true;
     }
 }
