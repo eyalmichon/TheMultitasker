441eb6721f9d3e5552aed08d93f629e44a320b10
Eyal <eyalmichon1@gmail.com>
2022-09-14 00:57:32 +0300

    small change: get help for owner commands.

--- handler/lib/commands/help.js
+++ handler/lib/commands/help.js
@@ -8,31 +8,40 @@ class Help {
 
     createHelp(commands) {
         let text = [`${b('Hello, these are the available commands:')}`];
+        let ownerText = text.slice()
         let sections = {};
         for (const [key, value] of Object.entries(commands)) {
-            if (!value.alias && value.type !== 'Owner')
+            if (!value.alias) {
                 if (!sections[value.type])
                     sections[value.type] = [key];
                 else
                     sections[value.type].push(key);
+            }
         }
         for (const [key, value] of Object.entries(sections)) {
-            text.push(`${b(key + ':')}\n${m(value.join(', '))}`)
+            if (key !== 'Owner')
+                text.push(`${b(key + ':')}\n${m(value.join(', '))}`)
+            ownerText.push(`${b(key + ':')}\n${m(value.join(', '))}`)
         }
         text.push(`${b(`Usage:`)} ${prefix}[command]\n\nFor more information about each command use ${prefix}help [command]`)
+        ownerText.push(`${b(`Usage:`)} ${prefix}[command]\n\nFor more information about each command use ${prefix}help [command]`)
         text.push(i('The Multitasker 🧙‍♂️'))
+        ownerText.push(i('The Multitasker 🧙‍♂️'))
 
-        return text.join('\n\n');
+        return [text.join('\n\n'), ownerText.join('\n\n')];
     }
 
     constructor(commands) {
-        this.helpText = this.createHelp(commands);
+        [this.helpText, this.ownerText] = this.createHelp(commands);
 
         commands.help = this.addInfo(this.help);
     }
 
     help = {
-        func: () => { return returnType.reply(this.helpText) },
+        func: (isOwner) => {
+            const text = isOwner ? returnType.reply(this.ownerText) : returnType.reply(this.helpText)
+            return text;
+        },
         help: async () => help.Help.help + await randomStory()
     }
 }
