d7dce50472eae47863193dae3b7faf797ce9bba7
Eyal <eyalmichon1@gmail.com>
2023-02-28 20:54:33 +0200

    remove all ai related content to a new sub section

--- handler/lib/commands/info.js
+++ handler/lib/commands/info.js
@@ -31,10 +31,6 @@ class Info {
         commands.tran = this.alias(this.translate)
         commands.tr = this.alias(this.translate)
 
-        commands.recognize = this.addInfo(this.recognizeMusic)
-        commands.rec = this.alias(this.recognizeMusic)
-        commands.rm = this.alias(this.recognizeMusic)
-
         commands.nikud = this.addInfo(this.nikud)
         commands.nik = this.alias(this.nikud)
         commands.נקד = this.alias(this.nikud)
@@ -53,9 +49,6 @@ class Info {
         commands.conjugate = this.addInfo(this.conjugate)
         commands.conj = this.alias(this.conjugate)
 
-        commands.thisdoesntexist = this.addInfo(this.thisDoesntExist)
-        commands.tde = this.alias(this.thisDoesntExist)
-
         commands.emojigenerator = this.addInfo(this.emojiGenerator)
         commands.randemoji = this.alias(this.emojiGenerator)
 
@@ -65,28 +58,7 @@ class Info {
 
         commands.currency = this.addInfo(this.curreny)
 
-        commands.imagine = this.addInfo(this.imagine)
-
-        commands.enhance = this.addInfo(this.enhanceImage)
-
         commands.poll = this.addInfo(this.poll)
-
-        commands.summarize = this.addInfo(this.summarize)
-        commands.sum = this.alias(this.summarize)
-
-        commands.topics = this.addInfo(this.topics)
-
-        commands.splitbysentence = this.addInfo(this.splitBySentence)
-        commands.sbs = this.alias(this.splitBySentence)
-
-        commands.anonymize = this.addInfo(this.anonymize)
-        commands.anon = this.alias(this.anonymize)
-
-        commands.htmlcontent = this.addInfo(this.htmlContent)
-        commands.html = this.alias(this.htmlContent)
-
-        commands.transcribe = this.addInfo(this.transcribe)
-        commands.tb = this.alias(this.transcribe)
     }
 
     compile = {
@@ -190,27 +162,6 @@ class Info {
         timer: () => this.defaultTimer
     }
 
-    recognizeMusic = {
-        func: async (message) => {
-            const options = parser.parse(message.args);
-
-            const full = !!options.f || !!options.full;
-            if (message.quotedMsg) message = message.quotedMsg;
-            if (!['ptt', 'audio', 'video'].includes(message.type)) return errors.WRONG_TYPE_RECO
-
-            const data = await decryptMedia(message);
-            return recognize.music(data)
-                .then(res => full ? returnType.reply(res.join('\n\n')) : returnType.reply(res[0]))
-                .catch(err => {
-                    console.error(err);
-                    if (err === 'NO_RESULT') return errors.NO_RESULT_RECO;
-                    return errors.UNKNOWN();
-                })
-        },
-        help: () => help.Info.recognizeMusic,
-        timer: () => this.defaultTimer
-    }
-
     nikud = {
         func: async (message) => {
             const options = parser.parse(message.args);
@@ -247,6 +198,7 @@ class Info {
         help: () => help.Info.grammar,
         timer: () => this.defaultTimer
     }
+
     tts = {
         func: (message) => {
             const options = parser.parse(message.args);
@@ -266,6 +218,7 @@ class Info {
         help: () => help.Info.tts,
         timer: () => this.defaultTimer
     }
+
     context = {
         func: (message) => {
             const options = parser.parse(message.args);
@@ -292,6 +245,7 @@ class Info {
         help: () => help.Info.context,
         timer: () => this.defaultTimer
     }
+
     synonym = {
         func: (message) => {
             const options = parser.parse(message.args);
@@ -310,6 +264,7 @@ class Info {
         help: () => help.Info.synonym,
         timer: () => this.defaultTimer
     }
+
     conjugate = {
         func: (message) => {
             const options = parser.parse(message.args);
@@ -332,43 +287,6 @@ class Info {
         help: () => help.Info.conjugate,
         timer: () => this.defaultTimer
     }
-    thisDoesntExist = {
-        func: (message) => {
-            const types = ['person', 'cat', 'horse', 'rental', 'waifu', 'question',
-                'chemical', 'word', 'city', 'simpsons', 'art', 'video', 'ideas', 'lyrics']
-            const options = parser.parse(message.args);
-            const hq = !!options.hq || !!options.hd;
-            let type = null;
-            types.forEach(t => {
-                if (options[t]) {
-                    type = t
-                    return;
-                }
-            })
-            if (!type)
-                type = types[Math.floor(Math.random() * types.length)]
-
-            return doesntExist.thisDoesntExist(type, options)
-                .then(result => {
-                    switch (result.type) {
-                        case 'text':
-                            return returnType.reply(result.info);
-                        case 'img':
-                            if (hq) return returnType.sendFile(`data:document/png;base64,${result.info}`, `the_multitasker_${options.joinedText}.png`, '', false)
-                            else return returnType.sendFile(`data:image/png;base64,${result.info}`, `the_multitasker_${options.joinedText}.png`, '', false)
-                        case 'videoLink':
-                            return downloader.video(result.info)
-                                .then(path => returnType.sendFile(path))
-                    }
-                })
-                .catch(err => {
-                    console.error(err);
-                    return errors.UNKNOWN();
-                })
-        },
-        help: () => help.Info.thisDoesntExist,
-        timer: () => this.defaultTimer
-    }
 
     emojiGenerator = {
         func: (message) => {
@@ -382,6 +300,7 @@ class Info {
         help: () => help.Info.emojiGenerator,
         timer: () => this.defaultTimer
     }
+
     qr = {
         func: (message) => {
             const options = parser.parse(message.args, false);
@@ -400,6 +319,7 @@ class Info {
         help: () => help.Info.qr,
         timer: () => this.defaultTimer
     }
+
     carInfo = {
         func: (message) => {
             return carInfo.infoByCarNumber(message.args[0])
@@ -418,6 +338,7 @@ class Info {
         help: () => help.Info.carInfo,
         timer: () => this.defaultTimer
     }
+
     curreny = {
         func: (message) => {
             const options = parser.parse(message.args)
@@ -431,90 +352,6 @@ class Info {
         },
         help: () => help.Info.currency + currency.getCurrenciesString(),
         timer: () => this.defaultTimer
-    }
-
-    imagine = {
-        func: async (message) => {
-            const options = parser.parse(message.args);
-
-            if (!options.joinedText) return errors.EMPTY_TEXT;
-
-            if (!!options.quality) {
-                options.num_inference_steps = options.quality;
-                delete options.quality;
-            }
-            if (!!options.freedom) {
-                options.guide_scale = options.freedom;
-                delete options.freedom;
-            }
-            if (!!options.ratio) {
-                options.aspect_ratio = options.ratio;
-                delete options.ratio;
-            }
-            if (!!options.v) {
-                options.model = options.v;
-                delete options.v;
-            }
-            if (message.type === 'image' || (message.quotedMsg && message.quotedMsg.type === 'image'))
-                options.image = true;
-            if (!!options.image) {
-                // if it's a url
-                if (options.url) {
-                    // 15MB limit
-                    await fetcher.checkSize(options.url, 15728640)
-                        .then(async res => {
-                            if (res === 'OK')
-                                options.init_image = await fetcher.fetchBase64(options.url)
-                            else
-                                console.error(`Error in imagine (checkSize): ${res}\n Url: ${options.url}`)
-                        })
-                }
-                // if it's an image.
-                else if (message.type === 'image')
-                    options.init_image = await decryptMedia(message).then(buffer => buffer.toString('base64'));
-                // if it's a quoted image.
-                else if (!!message.quotedMsg)
-                    if (message.quotedMsg.type === 'image') {
-                        options.init_image = await decryptMedia(message.quotedMsg).then(buffer => buffer.toString('base64'))
-                    }
-                if (!!options.strength)
-                    options.prompt_strength = options.strength;
-            }
-            if (!!options.neg)
-                options.negative_prompt = parser.parseStringForFlag(message.args, 'neg');
-
-            return imagine.textToImage(options.joinedText, options)
-                .then(result => returnType.fileFromURL(result.image_url, 'the_multitasker.png', options.joinedText))
-                .catch(err => {
-                    console.error(err);
-                    if (err === 'Insufficient permissions')
-                        return errors.INSUFFICIENT_PERMISSIONS_IMAGINE;
-
-                    return errors.UNKNOWN();
-                })
-        },
-        help: () => help.Info.imagine,
-        timer: () => 60
-    }
-
-    enhanceImage = {
-        func: async (message) => {
-            if (message.type !== 'image' && (!message.quotedMsg || message.quotedMsg.type !== 'image'))
-                return errors.NOT_IMG;
-
-            message = message.type === 'image' ? message : message.quotedMsg;
-
-            const base64 = await decryptMedia(message).then(buffer => buffer.toString('base64'));
-
-            return imagine.enhanceImage(base64)
-                .then(result => returnType.fileFromURL(result.image_url, 'the_multitasker.png', 'enhanced image'))
-                .catch(err => {
-                    console.error(err);
-                    return errors.UNKNOWN();
-                })
-        },
-        help: () => help.Info.enhanceImage,
-        timer: () => 60
     }
 
     poll = {
@@ -534,137 +371,6 @@ class Info {
         help: () => help.Info.poll,
         timer: () => this.defaultTimer
     }
-
-    summarize = {
-        func: (message) => {
-            const options = parser.parse(message.args);
-            const text = options.joinedText;
-            if (!text) return errors.EMPTY_TEXT;
-            const long = !!options.long;
-            if (!long && text.length > 10000) return errors.CONTENT_TOO_LARGE;
-            const maxLength = Math.abs(parseInt(options.max)) || 100;
-            const minLength = Math.abs(parseInt(options.min)) || 5;
-
-            return ai.summarize(text, { maxLength, minLength })
-                .then(result => returnType.reply(result))
-                .catch(err => {
-                    console.error(err);
-                    return errors.UNKNOWN();
-                }
-                )
-        },
-        help: () => help.Info.summarize,
-        timer: () => 60
-    }
-
-    topics = {
-        func: (message) => {
-            const options = parser.parse(message.args);
-            const text = options.joinedText;
-            if (!text) return errors.EMPTY_TEXT;
-            const long = !!options.long;
-            if (!long && text.length > 10000) return errors.CONTENT_TOO_LARGE;
-            const hash = !!options.hash;
-
-            return ai.topics(text)
-                .then(result => returnType.reply(hash ? result.map(topic => `#${topic}`).join(' ') : result.join(', ')))
-                .catch(err => {
-                    console.error(err);
-                    return errors.UNKNOWN();
-                }
-                )
-        },
-        help: () => help.Info.topics,
-        timer: () => 60
-    }
-
-    splitBySentence = {
-        func: (message) => {
-            const options = parser.parse(message.args);
-            const text = options.joinedText;
-            if (!text) return errors.EMPTY_TEXT;
-            const long = !!options.long;
-            if (!long && text.length > 10000) return errors.CONTENT_TOO_LARGE;
-
-            return ai.splitBySentence(text)
-                .then(result => returnType.reply(result.join('\n\n')))
-                .catch(err => {
-                    console.error(err);
-                    return errors.UNKNOWN();
-                }
-                )
-        },
-        help: () => help.Info.splitBySentence,
-        timer: () => 60
-    }
-
-    anonymize = {
-        func: (message) => {
-            const options = parser.parse(message.args);
-            const text = options.joinedText;
-            if (!text) return errors.EMPTY_TEXT;
-            const long = !!options.long;
-            if (!long && text.length > 10000) return errors.CONTENT_TOO_LARGE;
-
-            return ai.anonymize(text)
-                .then(result => returnType.reply(result))
-                .catch(err => {
-                    console.error(err);
-                    return errors.UNKNOWN();
-                }
-                )
-        },
-        help: () => help.Info.anonymize,
-        timer: () => 60
-    }
-    htmlContent = {
-        func: (message) => {
-            const options = parser.parse(message.args);
-            const url = options.url;
-            if (!url) return errors.EMPTY_TEXT;
-            const long = !!options.long;
-            if (!long && url.length > 10000) return errors.CONTENT_TOO_LARGE;
-
-            return ai.htmlContent(url)
-                .then(result => returnType.reply(result))
-                .catch(err => {
-                    console.error(err);
-                    return errors.UNKNOWN();
-                }
-                )
-        },
-        help: () => help.Info.htmlContent,
-        timer: () => 60
-    }
-
-    transcribe = {
-        func: async (message) => {
-            const options = parser.parse(message.args);
-            if (!message.quotedMsg) return errors.NO_QUOTED_MESSAGE;
-
-            if (!['ptt', 'audio'].includes(message.quotedMsg.type)) return errors.WRONG_TYPE_RECO;
-
-            const long = !!options.long;
-            if (!long && message.duration > 60) return errors.CONTENT_TOO_LARGE;
-
-            const data = await decryptMedia(message.quotedMsg);
-            if (!data) return errors.UNKNOWN();
-
-            const speakerDetection = options.speaker ? true : false;
-            const timestampPerLabel = options.ts ? true : false;
-
-            return ai.transcribe(data, { speakerDetection, timestampPerLabel })
-                .then(result => returnType.reply(result.join('\n\n')))
-                .catch(err => {
-                    console.error(err);
-                    return errors.UNKNOWN();
-                })
-        },
-        help: () => help.Info.transcribe,
-        timer: () => 60
-    }
-
-
 }
 
 module.exports = { Info }
